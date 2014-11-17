ac33caf754d4c9f5b0b4863d94136fe2772dc4dd
michael-hart <michael.hart1994@gmail.com>
2014-11-17 12:34:19 +0000

    Created template files for UART

---
+++ src/ROBOTC/common/uart.c
@@ -0,0 +1,10 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//	FILE: uart.c
+//	ORIGINAL AUTHOR: MH1613
+//	DESCRIPTION: This file is a ROBOTC source file. It allows input and output to and
+//  from UART1 on the Vex Cortex Microprocessor. This is vital for communication with
+//	the processor providing directions to the cortex.
+//  This file is common to both the large and the small robot.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
---
+++ src/ROBOTC/common/uart.h
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//	FILE: uart.h
+//	ORIGINAL AUTHOR: MH1613
+//	DESCRIPTION: This file is a ROBOTC headerfile. It allows input and output to and
+//  from UART1 on the Vex Cortex Microprocessor. This is vital for communication with
+//	the processor providing directions to the cortex.
+//  This file is common to both the large and the small robot.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _UART_H
+#define _UART_H
+
+#include "uart.c";
+
+#endif // _UART_H
