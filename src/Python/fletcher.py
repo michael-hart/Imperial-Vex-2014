fca73af1afb4e92569aa797195844077730ed3b1
michael-hart <michael.hart1994@gmail.com>
2015-01-28 15:57:29 +0000

    Removed use of inbuilt sum and bad import

--- src/Python/fletcher.py
+++ src/Python/fletcher.py
@@ -1,16 +1,16 @@
-from _int import *
+
 def compute(data_to_checksum, size, modulo, limit = None):
-    sum, sum_of_sum = 1, 0
+    _sum, sum_of_sum = 1, 0
     length = len(data_to_checksum)
     if limit is not None and length > limit:
         data_to_checksum = data_to_checksum[:limit]
     for char in data_to_checksum:
-        sum += ord(char)
-	sum_of_sum += sum
-	sum %= modulo
-	sum_of_sum += sum
+        _sum += ord(char)
+	sum_of_sum += _sum
+	_sum %= modulo
+	sum_of_sum += _sum
 	sum_of_sum %= modulo
-    return (sum_of_sum << (size / 2 )) + sum
+    return (sum_of_sum << (size / 2 )) + _sum
 
 def fletcher16(data_to_checksum):
     return compute(data_to_checksum, 8, limit=21)
