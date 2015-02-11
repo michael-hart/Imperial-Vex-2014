e6220149c6b53443d9e72ffcbdb8065b578867d1
Michael Hart <michael.hart1994@gmail.com>
2015-02-11 16:29:13 +0000

    Corrections to Fletcher checks and UART

--- src/Python/fletcher.py
+++ src/Python/fletcher.py
@@ -1,26 +1,26 @@
 
 def fletcher16(data_to_checksum):
-    """ Data in is assumed to be string characters, rather than numbers """
-    assert type(data_to_checksum[0]) == str
+    """ Data in is assumed an integer list """
+    assert type(data_to_checksum[0]) == int
     sum1, sum2 = 0, 0
     for i in data_to_checksum:
-        sum1 = (sum1 + ord(i)) % 255
+        sum1 = (sum1 + i) % 255
         sum2 = (sum2 + sum1) % 255
     return (sum2 << 8) | sum1
 	
 def compare_checksum(data_to_checksum, checksum):
     chk = fletcher16(data_to_checksum)
-    print "Checking: " + " ".join([hex(ord(x)) for x in data_to_checksum])
-    return chk == (ord(checksum[0]) << 8) + ord(checksum[1])
+    print "Checking: " + " ".join([hex(x) for x in data_to_checksum])
+    return chk == (checksum[0] << 8) + checksum[1]
 	
 	
 if __name__ == "__main__":
-    test_arr = map(chr, [1, 0x22, 0])
+    test_arr = [1, 0x22, 0]
     c16 = fletcher16(test_arr)
     # Results generated using C functions
     assert (c16 >> 8) == 0x47
     assert (c16 & 0xFF) == 0x23
-    test_arr += chr((c16 >> 8) & 0xFF)
-    test_arr += chr(c16 & 0xFF)
+    test_arr += [(c16 >> 8) & 0xFF]
+    test_arr += [c16 & 0xFF]
     assert compare_checksum(test_arr[:3], test_arr[3:])
     print "Test completed successfully"
--- src/Python/uart.py
+++ src/Python/uart.py
@@ -9,11 +9,26 @@ import fletcher
 ser = None
 continue_thread = True
 
+tx_heartbeat = 0x01
+tx_forward = 0x02
+tx_rotate = 0x04
+tx_acknowledge = 0x08
+tx_lift_height = 0x10
+tx_score = 0x20
+
+rx_heartbeat = 0x01
+rx_left_encoder = 0x02
+rx_right_encoder = 0x04
+rx_acknowledge = 0x08
+rx_wake_up = 0x10
+
+
 class UART:
     """ A class containing methods for RPi UART """
     
     def __init__(self, open_port = "/dev/ttyAMA0", baud_rate=115200):
         """ Opens given port in non-blocking mode at baud rate 115.2k """
+        # Set up serial and check that it works correctly
         self.serial = serial.Serial(port=open_port, baudrate=baud_rate, timeout=2)
         self.serial.close()
         self.serial.open()
@@ -21,14 +36,20 @@ class UART:
         atexit.register(self.cleanup)
         self.serial.flushInput()
         self.serial.flushOutput()
+        # Convert into a thread
         self.thread = threading.Thread(target=self.thread_poll, args=())
-	self.thread.daemon = True
+        self.thread.daemon = True
         self.thread.start()
+        # Set up object variables
         self.buffer = []
-
-        # Check length of command list to see if data is waiting
         self.command_list = []
         self.command_lock = threading.Lock()
+        self.send_buffer = []
+        self.send_buffer_lock = threading.Lock()
+        self.ack_buffer = []
+
+        self.last_heartbeat_sent = time.time()
+        self.current_packet_id = 0
     
     def write(self, s):
         """ Writes the string s to the serial port """
@@ -42,27 +63,56 @@ class UART:
 	print "Stopped due to continue_thread is False"
         
     def poll(self):
+        # Check for thread killing
     	if continue_thread == False:
     		cleanup()
     		return
+        # Read and process any incoming data
         waiting = self.serial.inWaiting()
         if waiting > 0:
             # list does conversion from bytes, which is string, to list of characters
             self.buffer += list(self.serial.read(waiting))
-            # Check fletcher of first five bytes
-            if len(self.buffer) > 4:
-                if fletcher.compare_checksum(self.buffer[:3], self.buffer[3:5]):
-                    # Fletcher checks out, append to command list and delete from buffer
-                    self.add_command_thread_safe(self.buffer[:5])
-                    self.buffer = self.buffer[5:]
-                else:
-                    # Fletcher not checked out. Either packet is corrupt or we're out of sync
-                    # Iterate over list until a valid checksum appears and destroy previous data.
-                    for i in range(len(self.buffer)-4):
-                        if fletcher.compare_checksum(self.buffer[i:i+3], self.buffer[i+3:i+5]):
-                            self.add_command_thread_safe(self.buffer[i:i+5])
-                            self.buffer = self.buffer[i+5:]
-                            break
+            self.process_rcv_buffer()
+
+        # Check for last hearbeat, and send new one if necessary
+        if time.time() - last_heartbeat_sent > 0.03:
+            last_heartbeat_sent = time.time()
+            self.heartbeat()
+
+        # Write any waiting commands and acknowledges
+        # First, wait for lock
+        while self.send_buffer_lock.locked():
+            time.sleep(.001)
+        with self.send_buffer_lock:
+            # Iterate over list and send each command
+            for cmd in self.send_buffer:
+                # Do we need an acknowledge?
+                if (cmd[0] == 0x2 or cmd[0] == 0x4 or cmd[0] ==  0x10):
+                    ack_buffer.append( (cmd, time.time()) )
+                # Send!
+                self.serial.write(''.join(cmd))
+
+
+    def process_rcv_buffer(self):
+        """ Uses self.buffer ONLY. """
+        # Check fletcher of first five bytes
+        if len(self.buffer) > 4:
+            if fletcher.compare_checksum(self.buffer[:3], self.buffer[3:5]):
+                # Fletcher checks out, append to command list and delete from buffer
+                if (self.buffer[0] == rx_wake_up or self.buffer[0] == rx_left_encoder or self.buffer[0] == rx_right_encoder):
+                    acknowledge(buffer[1])
+                self.add_command_thread_safe(self.buffer[:5])
+                self.buffer = self.buffer[5:]
+            else:
+                # Fletcher not checked out. Either packet is corrupt or we're out of sync
+                # Iterate over list until a valid checksum appears and destroy previous data.
+                for i in range(len(self.buffer)-4):
+                    if fletcher.compare_checksum(self.buffer[i:i+3], self.buffer[i+3:i+5]):
+                        if (self.buffer[i] == rx_wake_up or self.buffer[i] == rx_left_encoder or self.buffer[i] == rx_right_encoder):
+                            acknowledge(buffer[i+1])
+                        self.add_command_thread_safe(self.buffer[i:i+5])
+                        self.buffer = self.buffer[i+5:]
+                        break
 
     def add_command_thread_safe(self, cmd):
         while self.command_lock.locked():
@@ -91,6 +141,37 @@ class UART:
             cmd_copy = list(self.command_list)
             self.command_list = []
         return cmd_copy
+
+    # Define all commands that can be sent
+    def build_command(self, cmd, data):
+        # Build packet
+        packet = [cmd, self.current_packet_id, data]
+        self.current_packet_id = (self.current_packet_id + 1) % 255
+        checksum = fletcher.fletcher16(packet)
+        packet += [(checksum >> 8) & 0xFF, checksum & 0xFF]
+        # Thread-safe add to buffer
+        while self.send_buffer_lock.locked():
+            time.sleep(0.001)
+        with self.send_buffer_lock:
+            self.send_buffer.append(packet)
+
+    def heartbeat(self):
+        self.build_command(tx_heartbeat, 0x00)
+
+    def forward(self, data):
+        self.build_command(tx_forward, data)
+
+    def rotate(self, data):
+        self.build_command(tx_rotate, data)
+
+    def acknowledge(self, data):
+        self.build_command(tx_acknowledge, data)
+
+    def score(self):
+        self.build_command(tx_score, 0x00)
+
+    def lift_height(self, data):
+        self.build_command(tx_lift_height, data)
     
     def cleanup(self):
 	continue_thread = False
